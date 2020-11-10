190fe7bde8571ffc056f053ca73aae3cecd09708
Albert <pyro_quiro@hotmail.com>
2020-11-10 15:13:44 +0000

    Added typing to two methods

--- easyglue/__init__.py
+++ easyglue/__init__.py
@@ -21,24 +21,6 @@ class EasyDynamicFrameReader:
     def __init__(self, glue_context: GlueContext):
         self.glue_context = glue_context
 
-    def _read_from_s3(self, data_format: str, s3_paths: str = "", transformation_ctx: str = "",
-                      push_down_predicate: str = "", **kwargs: Any):
-        if s3_paths:
-            if isinstance(s3_paths, str):
-                self.connection_options_dict['paths'] = [s3_paths]
-            elif isinstance(s3_paths, list):
-                self.connection_options_dict['paths'] = s3_paths
-            # TODO Add else with an error message if s3_path is not str or list
-
-        return self.glue_context.create_dynamic_frame.from_options(connection_type='s3',
-                                                                   connection_options=self.connection_options_dict,
-                                                                   format=data_format,
-                                                                   format_options=self.format_options_dict,
-                                                                   transformation_ctx=transformation_ctx,
-                                                                   push_down_predicate=push_down_predicate,
-                                                                   kwargs=kwargs
-                                                                   )
-
     def csv(self, s3_paths, transformation_ctx: str = "", push_down_predicate: str = "",
             **kwargs: Any) -> DynamicFrame:
         return self._read_from_s3(data_format='csv', s3_paths=s3_paths, transformation_ctx=transformation_ctx,
@@ -84,8 +66,26 @@ class EasyDynamicFrameReader:
         return self._read_from_s3(data_format='xml', s3_paths=s3_paths, transformation_ctx=transformation_ctx,
                                   push_down_predicate=push_down_predicate, kwargs=kwargs)
 
+    def _read_from_s3(self, data_format: str, s3_paths: str = "", transformation_ctx: str = "",
+                      push_down_predicate: str = "", **kwargs: Any) -> DynamicFrame:
+        if s3_paths:
+            if isinstance(s3_paths, str):
+                self.connection_options_dict['paths'] = [s3_paths]
+            elif isinstance(s3_paths, list):
+                self.connection_options_dict['paths'] = s3_paths
+            # TODO Add else with an error message if s3_path is not str or list
+
+        return self.glue_context.create_dynamic_frame.from_options(connection_type='s3',
+                                                                   connection_options=self.connection_options_dict,
+                                                                   format=data_format,
+                                                                   format_options=self.format_options_dict,
+                                                                   transformation_ctx=transformation_ctx,
+                                                                   push_down_predicate=push_down_predicate,
+                                                                   kwargs=kwargs
+                                                                   )
+
     def table(self, database_name: str, table_name: str, redshift_tmp_dir: str = "", transformation_ctx: str = "",
-              push_down_predicate: str = "", catalog_id: int = None, **kwargs: Any):
+              push_down_predicate: str = "", catalog_id: int = None, **kwargs: Any) -> DynamicFrame:
         return self.glue_context.create_dynamic_frame.from_catalog(database=database_name,
                                                                    table_name=table_name,
                                                                    redshift_tmp_dir=redshift_tmp_dir,
