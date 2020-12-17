e6571e86627f12d1a83b4f22e84b572fe492e2ff
Albert <pyro_quiro@hotmail.com>
2020-12-17 13:50:20 +0000

    Added error handling for non-string values in s3_paths

--- easyglue/__init__.py
+++ easyglue/__init__.py
@@ -1,4 +1,6 @@
-from typing import Any
+import sys
+from typing import Any, Union
+from urllib.parse import urlparse
 
 from awsglue.context import GlueContext
 from awsglue.dynamicframe import DynamicFrame
@@ -66,14 +68,17 @@ class EasyDynamicFrameReader:
         return self._read_from_s3(data_format='xml', s3_paths=s3_paths, transformation_ctx=transformation_ctx,
                                   push_down_predicate=push_down_predicate, kwargs=kwargs)
 
-    def _read_from_s3(self, data_format: str, s3_paths: str = "", transformation_ctx: str = "",
+    def _read_from_s3(self, data_format: str, s3_paths: Union[str, list] = "", transformation_ctx: str = "",
                       push_down_predicate: str = "", **kwargs: Any) -> DynamicFrame:
         if s3_paths:
             if isinstance(s3_paths, str):
                 self.connection_options_dict['paths'] = [s3_paths]
             elif isinstance(s3_paths, list):
                 self.connection_options_dict['paths'] = s3_paths
-            # TODO Add else with an error message if s3_path is not str or list
+            else:
+                self.glue_context.get_logger().error(f'Attribute "s3_paths" must be either str or list, '
+                                                     f'{type(s3_paths)} was provided instead')
+                sys.exit(1)
 
         return self.glue_context.create_dynamic_frame.from_options(connection_type='s3',
                                                                    connection_options=self.connection_options_dict,
@@ -84,8 +89,8 @@ class EasyDynamicFrameReader:
                                                                    kwargs=kwargs
                                                                    )
 
-    def table(self, database_name: str, table_name: str, redshift_tmp_dir: str = "", transformation_ctx: str = "",
-              push_down_predicate: str = "", catalog_id: int = None, **kwargs: Any) -> DynamicFrame:
+    def catalog(self, database_name: str, table_name: str, redshift_tmp_dir: str = "", transformation_ctx: str = "",
+                push_down_predicate: str = "", catalog_id: int = None, **kwargs: Any) -> DynamicFrame:
         return self.glue_context.create_dynamic_frame.from_catalog(database=database_name,
                                                                    table_name=table_name,
                                                                    redshift_tmp_dir=redshift_tmp_dir,
