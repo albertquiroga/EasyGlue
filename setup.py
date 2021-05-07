c4d776aac521dcfd41a8ece75abadf5cdf289e3d
Albert Quiroga <albertquirogabertolin@gmail.com>
2021-05-07 10:30:22 +0100

    Version bump to 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="easyglue",
-    version="0.0.6",
+    version="0.0.7",
     author="Albert Quiroga",
     author_email="albertquirogabertolin@gmail.com",
     description="Glue DynamicFrame syntax closer to DataFrames",
