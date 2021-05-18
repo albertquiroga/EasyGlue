3aba5b69886f722d63d1515dcde208c6caa7c905
Albert Quiroga <albertquirogabertolin@gmail.com>
2021-05-18 12:48:00 +0100

    Added missing documentation to writer mixins CatalogMixin and S3Mixin

--- easyglue/writer/_CatalogMixin.py
+++ easyglue/writer/_CatalogMixin.py
@@ -15,6 +15,16 @@ class CatalogMixin:
     @writer_method
     def _write_to_catalog(self, database: str, table: str, redshift_tmp_dir: str = "", transformation_ctx: str = "",
                           catalog_id: int = None, **kwargs: Any):
+        """
+        Writes the DynamicFrame to catalog by invoking write_dynamic_frame.from_catalog with the right options
+        :param database: Data catalog database to write to
+        :param table: Data catalog table to write to
+        :param redshift_tmp_dir: Redshift temporary path
+        :param transformation_ctx: Glue transformation context
+        :param catalog_id: Glue Data Catalog ID
+        :param kwargs: Keyword arguments
+        :return: None
+        """
         self.glue.write_dynamic_frame.from_catalog(frame=self.dyf,
                                                    database=database,
                                                    table_name=table,
@@ -26,11 +36,31 @@ class CatalogMixin:
 
     def catalog(self, database: str, table: str, redshift_tmp_dir: str = "", transformation_ctx: str = "",
                 catalog_id: int = None, **kwargs: Any):
+        """
+        Writes the DynamicFrame to catalog by invoking _write_to_catalog
+        :param database: Data catalog database to write to
+        :param table: Data catalog table to write to
+        :param redshift_tmp_dir: Redshift temporary path
+        :param transformation_ctx: Glue transformation context
+        :param catalog_id: Glue Data Catalog ID
+        :param kwargs: Keyword arguments
+        :return: None
+        """
         self._write_to_catalog(database=database, table=table, redshift_tmp_dir=redshift_tmp_dir,
                                transformation_ctx=transformation_ctx, catalog_id=catalog_id, kwargs=kwargs)
 
     def table(self, qualified_name: str, redshift_tmp_dir: str = "", transformation_ctx: str = "",
               catalog_id: int = None, **kwargs: Any):
+        """
+        Writes the DynamicFrame to catalog by invoking _write_to_catalog
+        :param database: Data catalog database to write to
+        :param table: Data catalog table to write to
+        :param redshift_tmp_dir: Redshift temporary path
+        :param transformation_ctx: Glue transformation context
+        :param catalog_id: Glue Data Catalog ID
+        :param kwargs: Keyword arguments
+        :return: None
+        """
         database_name, table_name = validate_qualified_name(qualified_name)
         self._write_to_catalog(database=database_name, table=table_name, redshift_tmp_dir=redshift_tmp_dir,
                                transformation_ctx=transformation_ctx, catalog_id=catalog_id, kwargs=kwargs)
